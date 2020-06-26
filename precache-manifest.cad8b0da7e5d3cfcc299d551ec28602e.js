88016b848cdc2ee7239f98ea162e2fcd8f838cc3
Ganesh <mandapatiganesh389@gmail.com>
2020-06-26 13:19:14 +0530

    Updates

---
+++ precache-manifest.cad8b0da7e5d3cfcc299d551ec28602e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb57fc27b2401ba47ebeb43665867281",
+    "url": "/GaneshMadapati/ReactFoodSearchApp/index.html"
+  },
+  {
+    "revision": "66bae4e0d8195e900581",
+    "url": "/GaneshMadapati/ReactFoodSearchApp/static/css/main.dddc89e2.chunk.css"
+  },
+  {
+    "revision": "d310b8de8575d8f79fa3",
+    "url": "/GaneshMadapati/ReactFoodSearchApp/static/js/2.dd023780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GaneshMadapati/ReactFoodSearchApp/static/js/2.dd023780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bae4e0d8195e900581",
+    "url": "/GaneshMadapati/ReactFoodSearchApp/static/js/main.5ffd95d4.chunk.js"
+  },
+  {
+    "revision": "caa97990744d6f883c21",
+    "url": "/GaneshMadapati/ReactFoodSearchApp/static/js/runtime-main.44c066d0.js"
+  }
+]);
